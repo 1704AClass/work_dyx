da4dfe38666c5ade8ac75614fdd079f6adcee145
duanyinxing <1269911577@qq.com>
2020-01-23 13:05:31 +0800

    第九天

--- health_service_provider/src/main/java/com/itheima/service/impl/MemberServiceImpl.java
+++ health_service_provider/src/main/java/com/itheima/service/impl/MemberServiceImpl.java
@@ -8,6 +8,9 @@ import com.itheima.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 会员服务
  */
@@ -30,5 +33,16 @@ public class MemberServiceImpl implements MemberService {
             member.setPassword(password);
         }
         memberDao.add(member);
+    }
+
+    //根据月份查询会员数量
+    public List<Integer> findMemberCountByMonths(List<String> months) {//2018.05
+        List<Integer> memberCount = new ArrayList<>();
+        for (String month : months) {
+            String date = month + ".31";//2018.05.31
+            Integer count = memberDao.findMemberCountBeforeDate(date);
+            memberCount.add(count);
+        }
+        return memberCount;
     }
 }
